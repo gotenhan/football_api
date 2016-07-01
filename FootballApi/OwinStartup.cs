8b848372182476898f8e6d3e53b2c2c6ac8619f3
Adrian Dyliński <gotenhan@gmail.com>
2016-07-01 16:13:12 +0100

    Initialize project

---
+++ FootballApi/OwinStartup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using FootballApi.Startup;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(FootballApi.OwinStartup))]
+
+namespace FootballApi
+{
+    public class OwinStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            WebApiConfiguration.Configure(app);
+        }
+    }
+}
---
+++ FootballApi/Startup/WebApiConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Owin;
+
+namespace FootballApi.Startup
+{
+    public class WebApiConfiguration
+    {
+        public static void Configure(IAppBuilder appBuilder)
+        {
+            var httpConfiguration = new HttpConfiguration();
+            ConfigureJsonFormatter(httpConfiguration);
+            MapRoutes(httpConfiguration);
+            appBuilder.UseWebApi(httpConfiguration);
+        }
+
+        private static void MapRoutes(HttpConfiguration httpConfiguration)
+        {
+            httpConfiguration.Routes.MapHttpRoute(
+                name: "Default Api",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new {id = RouteParameter.Optional});
+        }
+
+        private static void ConfigureJsonFormatter(HttpConfiguration httpConfiguration)
+        {
+            var jsonMediaTypeFormatter = httpConfiguration.Formatters.JsonFormatter;
+            jsonMediaTypeFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            jsonMediaTypeFormatter.SerializerSettings.Culture = CultureInfo.GetCultureInfo("en-GB");
+        }
+    }
+}
