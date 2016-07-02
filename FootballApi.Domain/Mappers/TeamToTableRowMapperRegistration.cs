26a8b7be80d6ffa485dd2e97ef0b77ae7617bd5b
Adrian Dyliński <gotenhan@gmail.com>
2016-07-02 12:27:04 +0100

    Fix mapping for GoalsDifference

--- FootballApi.Domain/Mappers/TeamToTableRowMapperRegistration.cs
+++ FootballApi.Domain/Mappers/TeamToTableRowMapperRegistration.cs
@@ -11,7 +11,8 @@ namespace FootballApi.Domain.Mappers
                 .Member(tr => tr.GoalsFor, t => t.GoalsScored)
                 .Member(tr => tr.GoalsAgainst, t => t.GoalsLost)
                 .Member(tr => tr.Points, t => t.Score)
-                .Member(tr => tr.Team, t => t.Name);
+                .Member(tr => tr.Team, t => t.Name)
+                .Member(tr => tr.GoalDifference, t => t.GoalsDifference);
         }
     }
 }
